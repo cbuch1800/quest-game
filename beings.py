cca9216bacebeded47b6aee0134b2bf333cad253
Craig Buchan <cbuchan1800@gmail.com>
2018-05-24 09:46:05 +0400

    created classes for players to use

--- beings.py
+++ beings.py
@@ -1,49 +1,102 @@
-class Being():
-    'Base class for all living characters in the game'
+from string import capwords
 
-    name = ""
-    home = ""
-    vitality = 0
-    dexterity = 0
-    agility = 0
-    experience = 0
-    # INIT AND GET/SET/CHANGE FOR EXPERIENCE
+class Being():
+    'Abstract base class for all living characters in the game'
+
+    _being_count = 0
+
+    # Private attributes:
+    # _name 
+    # _home
+    # _vitality
+    # _dexterity
+    # _agility 
+    # _experience
 
     def __init__(self, name, home):
-        self.name = name
-        self.home = home
-        self.experience = 1
+        Being._being_count += 1
+        self._name = capwords(name)
+        self._home = capwords(home)
+        self._vitality = 0
+        self._dexterity = 0
+        self._agility = 0
+        self._experience = 1
     
     def get_full_name(self):
-        return self.name + " of " + self.home
+        return self._name + " of " + self._home
 
     def get_vitality(self):
-        return self.vitality
+        return self._vitality
 
     def set_vitality(self, vitality):
-        self.vitality = vitality
+        self._vitality = vitality
 
     def get_dexterity(self):
-        return self.dexterity
+        return self._dexterity
 
     def set_dexterity(self, dexterity):
-        self.dexterity = dexterity
+        self._dexterity = dexterity
 
     def get_agility(self):
-        return self.agility
+        return self._agility
 
     def set_agility(self, agility):
-        self.agility = agility
+        self._agility = agility
+
+    def get_experience(self):
+        return self._experience
+
+    def set_experience(self, experience):
+        self._experience = experience
 
 
 class Human(Being):
 
-    def __init__(self, name, home, vitality, dexterity, agility):
+    _human_count = 0
+
+    def __init__(self, name, home):
         super().__init__(name, home)
-        self.vitality = 5
-        self.dexterity = 5
-        self.agility = 5
+        Human._human_count += 1
+        self._vitality = 5
+        self._dexterity = 5
+        self._agility = 5
 
 
-myBeing = Human("Craig", "Aberdeen")
+class Gnome(Being):
+
+    _gnome_count = 0
+
+    def __init__(self, name, home):
+        super().__init__(name, home)
+        Gnome._gnome_count += 1
+        self._vitality = 4
+        self._dexterity = 5
+        self._agility = 6
+
+
+class Cyclops(Being):
+
+    _cyclops_count = 0
+
+    def __init__(self, name, home):
+        super().__init__(name, home)
+        Cyclops._cyclops_count += 1   
+        self._vitality = 6
+        self._dexterity = 4
+        self._agility = 5
+
+
+class Elf(Being):
+
+    _elf_count = 0
+
+    def __init__(self, name, home):
+        super().__init__(name, home)
+        Elf._elf_count += 1   
+        self._vitality = 5
+        self._dexterity = 6
+        self._agility = 4
+
+
+myBeing = Human("crAig", "Aberdeen")
 print(myBeing.get_full_name())
